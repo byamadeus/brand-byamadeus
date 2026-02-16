5e83c9e4f28e0dbc818790d9fdf84274ab4aaa4f
byamadeus <evanamadeuscameron@gmail.com>
2026-02-16 16:42:27 -0500

    error on the nav

--- types/web-components.d.ts
+++ types/web-components.d.ts
@@ -1,9 +1,13 @@
 // types/web-components.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    'portfolio-nav': React.DetailedHTMLProps
-      React.HTMLAttributes<HTMLElement>, 
-      HTMLElement
-    >;
+import React from 'react';
+
+declare global {
+  namespace React.JSX {
+    interface IntrinsicElements {
+      'portfolio-nav': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
   }
 }
